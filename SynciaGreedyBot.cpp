be83bc7856c4ef3288c49ca26eef576860ae10c1
s.okada <kokuzen@gmail.com>
2012-11-21 23:13:50 +0900

    first commit.

---
+++ SynciaGreedyBot.cpp
@@ -0,0 +1,112 @@
+#ifdef SYNCIAGREEDYBOT_UNIT_TEST
+#include "SynciaGreedyBot.h"
+#include <iostream>
+
+using namespace syncia;
+
+int main(int argc, char* argv[])
+{
+	boost::asio::io_service service;
+	boost::asio::io_service::work w(service);
+	boost::thread_group thread_group;
+	int thread_num = 5;
+	for(int i = 0; i < thread_num; ++i){
+		thread_group.create_thread(boost::bind(&boost::asio::io_service::run, &service));
+	}
+	int local_port = 54321;
+	auto hostname = std::string("localhost");
+	if(argc > 1){
+		hostname = std::string(argv[1]);
+		if(argc == 3)
+		{
+			local_port = boost::lexical_cast<int>(std::string(argv[2]));
+		}	
+	}
+	auto node_id = neuria::network::CreateSocketNodeId(hostname, local_port);
+	std::cout << "NodeId is " << node_id << std::endl; 
+
+	const int buffer_size = 128;
+	const unsigned int max_key_hash_count = 30;
+	const unsigned int spread_key_hash_max_count = 200;
+	const unsigned int max_hop_count = 6;
+
+	std::stringstream no_output;
+	std::ostream& os = /*no_output;*/std::cout;
+
+	auto server = neuria::network::SocketServer::Create(
+		service, local_port, buffer_size, os);
+	auto dispatcher = BehaviorDispatcher::Create(service, os);
+
+	auto client = neuria::network::SocketClient::Create(
+		service, buffer_size, os);
+	
+	auto upper_session_pool = neuria::network::SessionPool::Create();
+	auto lower_session_pool = neuria::network::SessionPool::Create();
+	auto file_db = database::FileKeyHashDb::Create(0.3, buffer_size, std::cout);
+	auto searched_file_db = 
+		database::FileKeyHashDb::Create(0.3, buffer_size, std::cout);
+
+	auto syncia = SynciaGreedyBot::Create(
+		max_key_hash_count, spread_key_hash_max_count, max_hop_count, buffer_size, 
+		upper_session_pool, lower_session_pool, file_db, searched_file_db, 
+		node_id, std::cout);
+	syncia->Bind(client);
+	syncia->Bind(dispatcher);
+	auto multiple_timer = neuria::timer::MultipleTimer::Create(service);
+	syncia->Bind(multiple_timer);
+	syncia->Initialize();
+
+	auto shell = neuria::test::CuiShell(std::cout);
+	neuria::test::RegisterExitFunc(shell);
+	
+	shell.Register("upload", "add upload directory.", 
+		[syncia](const neuria::test::CuiShell::ArgList& args){
+			syncia->AddUploadDirectory(FileSystemPath(args.at(1)));
+		});
+	shell.Register("db", "show uploaded files.", 
+		[file_db](const neuria::test::CuiShell::ArgList& args){
+			std::cout << file_db << std::endl;
+		});
+	shell.Register("sdb", "show uploaded files.", 
+		[searched_file_db](const neuria::test::CuiShell::ArgList& args){
+			std::cout << searched_file_db << std::endl;
+		});
+	
+	
+	shell.Register("request", "hostname port id downloadpath: request file.", 
+		[syncia, file_db](const neuria::test::CuiShell::ArgList& args){
+			const auto file_key_hash = 
+				file_db->Get(boost::lexical_cast<unsigned int>(args.at(1)));
+			syncia->RequestFile(file_key_hash);
+		});
+	
+	shell.Register("upper", "show upper linked sessions.", 
+		[upper_session_pool](const neuria::test::CuiShell::ArgList& args){
+			std::cout << "upper session: " << upper_session_pool << std::endl;
+		});
+	shell.Register("lower", "show lower linked sessions.", 
+		[lower_session_pool](const neuria::test::CuiShell::ArgList& args){
+			std::cout << "lower session: " << lower_session_pool << std::endl;
+		});
+	
+	server->StartAccept(
+		neuria::network::Server::OnAcceptedFunc([dispatcher](
+				neuria::network::Session::Pointer session){
+			session->StartReceive(dispatcher->GetOnReceivedFunc());
+		}),
+		neuria::network::Server::OnFailedAcceptFunc([](
+				const neuria::network::ErrorCode){
+			//nothing
+		}),
+		neuria::network::Session::OnClosedFunc([](
+				neuria::network::Session::Pointer){
+			//nothing
+		})
+	);
+	shell.Start();
+	thread_group.join_all();
+
+    return 0;
+}
+
+#endif
---
+++ SynciaGreedyBot.h
@@ -0,0 +1,195 @@
+#pragma once
+//SynciaGreedyBot:20120926
+#include <iostream>
+#include "syncia/Syncia.h"
+
+namespace syncia
+{
+
+class SynciaGreedyBot : public boost::enable_shared_from_this<SynciaGreedyBot>{
+public:
+	using Pointer = boost::shared_ptr<SynciaGreedyBot>;
+
+	static auto Create(
+			unsigned int max_key_hash_count, 
+			unsigned int spread_key_hash_max_count, 
+			unsigned int max_hop_count, 
+			unsigned int buffer_size,
+			neuria::network::SessionPool::Pointer upper_session_pool,
+			neuria::network::SessionPool::Pointer lower_session_pool,
+			database::FileKeyHashDb::Pointer file_db, 
+			database::FileKeyHashDb::Pointer searched_file_db, 
+			const neuria::network::NodeId& node_id, 
+			std::ostream& os) -> Pointer {
+		auto upload_action = UploadAction::Create(file_db, node_id, os);
+		
+		auto fetch_link_behavior = LinkBehavior::Create(lower_session_pool, 
+			command::GetCommandId<command::LinkForFetchKeyHashCommand>(), os);
+
+		auto search_key_hash_behavior = SearchKeyHashBehavior::Create(
+			node_id, max_key_hash_count, max_hop_count, 
+			upper_session_pool, file_db, searched_file_db, os);
+	
+		auto spread_key_hash_action = 
+			SpreadKeyHashAction::Create(lower_session_pool, node_id, os);
+		auto spread_key_hash_behavior = SpreadKeyHashBehavior::Create(
+			node_id, spread_key_hash_max_count, max_hop_count, 
+			lower_session_pool, file_db, os);
+
+		auto request_file_action = RequestFileAction::Create(FileSystemPath("./updownload"), os);
+		auto request_file_behavior = RequestFileBehavior::Create(
+			buffer_size, file_db, os);
+
+		return Pointer(new SynciaGreedyBot(upload_action, 
+			fetch_link_behavior, 
+			search_key_hash_behavior,
+			spread_key_hash_action, spread_key_hash_behavior,
+			request_file_action, request_file_behavior,
+			file_db, node_id, 
+			os));
+	}
+
+	auto Initialize() -> void {
+		this->AddUploadDirectory(syncia::FileSystemPath("./updownload"));
+	}
+
+	auto SetOnReceivedSearchKeyHashAnswerFunc(
+			SearchKeyHashBehavior::OnReceivedAnswerFunc on_received_func) -> void {
+		this->search_key_hash_behavior->SetOnReceivedAnswerFunc(on_received_func);
+	}
+
+	auto SetOnRepliedFileFunc(
+			RequestFileAction::OnRepliedFileFunc on_replied_file_func) -> void {
+		this->request_file_action->SetOnRepliedFileFunc(on_replied_file_func);
+	}
+
+	auto SetDownloadDirectoryPath(const FileSystemPath& path) -> void {
+		this->request_file_action->SetDownloadDirectoryPath(path);
+	}
+
+	auto AddUploadDirectory(const FileSystemPath& upload_directory_path) -> void {
+		auto watcher = 
+			filesystem::AddRemoveFileInDirectoryWatcher::Create(upload_directory_path);
+		filesystem::SetOnAddedFileFunc(watcher, 
+			[this](const FileSystemPath& file_path){
+				this->shared_from_this()->upload_action->UploadFile(file_path);
+			}
+		);
+		filesystem::SetOnRemovedFileFunc(watcher,
+			[this](const FileSystemPath& file_path){
+				this->shared_from_this()->file_db->Erase(file_path);
+			}
+		);
+		//this->add_remove_watcher_list.push_back(watcher);
+		this->multiple_timer->AddCallbackFuncAndStartTimer(
+			5,
+			[watcher]() -> neuria::timer::IsContinue {
+				watcher->Check();
+				watcher->Call();
+				watcher->Update();
+				return neuria::timer::IsContinue(true);
+			}
+		);
+	}
+
+	auto RequestFile(const database::FileKeyHash& key_hash) -> void {
+		this->request_file_action->RequestFile(key_hash);
+	}
+
+	auto Bind(neuria::network::Client::Pointer client) -> void {
+		this->fetch_link_behavior->Bind(client);
+		this->search_key_hash_behavior->Bind(client);
+		this->spread_key_hash_behavior->Bind(client);
+		this->request_file_action->Bind(client);
+	}
+	
+	auto Bind(BehaviorDispatcher::Pointer dispatcher) -> void {
+		this->fetch_link_behavior->Bind(dispatcher);
+		this->search_key_hash_behavior->Bind(dispatcher);
+		this->spread_key_hash_behavior->Bind(dispatcher);
+		this->request_file_behavior->Bind(dispatcher);
+	}
+	
+	auto Bind(neuria::timer::MultipleTimer::Pointer multiple_timer) -> void {
+		this->multiple_timer = multiple_timer;
+		this->multiple_timer->AddCallbackFuncAndStartTimer(
+			10, 
+			[this]() -> neuria::timer::IsContinue {
+				this->file_db->Apply(
+					[this](database::FileKeyHash& key_hash){ 
+						if(key_hash.GetOwnerId() == this->node_id){
+							key_hash.SetBirthTimeNow();
+						}
+					}
+				);
+				return neuria::timer::IsContinue(true);
+			}
+		);
+		this->multiple_timer->AddCallbackFuncAndStartTimer(
+			10, 
+			[this]() -> neuria::timer::IsContinue {
+				this->spread_key_hash_action->RequestSpreadKeyHash();
+				return neuria::timer::IsContinue(true);
+			}
+		);
+		this->multiple_timer->AddCallbackFuncAndStartTimer(
+			30, 
+			[this]() -> neuria::timer::IsContinue {
+				for(const auto key_hash : this->file_db->GetFileKeyHashList()){
+					this->RequestFile(key_hash);
+				}
+				return neuria::timer::IsContinue(true);
+			}
+		);
+
+	}
+
+private:
+    SynciaGreedyBot(
+		UploadAction::Pointer upload_action, 
+		LinkBehavior::Pointer fetch_link_behavior,
+		SearchKeyHashBehavior::Pointer search_key_hash_behavior, 
+		SpreadKeyHashAction::Pointer spread_key_hash_action, 
+		SpreadKeyHashBehavior::Pointer spread_key_hash_behavior, 
+		RequestFileAction::Pointer request_file_action,
+		RequestFileBehavior::Pointer request_file_behavior,
+		database::FileKeyHashDb::Pointer file_db,
+		const neuria::network::NodeId& node_id, 
+		std::ostream& os) 
+			:upload_action(upload_action), 
+			fetch_link_behavior(fetch_link_behavior), 
+			search_key_hash_behavior(search_key_hash_behavior),
+			spread_key_hash_action(spread_key_hash_action),
+			spread_key_hash_behavior(spread_key_hash_behavior),
+			request_file_action(request_file_action),
+			request_file_behavior(request_file_behavior),
+			file_db(file_db), node_id(node_id), 
+			os(os){
+
+		
+	}
+
+	neuria::timer::MultipleTimer::Pointer multiple_timer;
+	std::vector<filesystem::AddRemoveFileInDirectoryWatcher> add_remove_watcher_list;
+
+	UploadAction::Pointer upload_action;
+	
+	LinkBehavior::Pointer fetch_link_behavior;
+
+	SearchKeyHashAction::Pointer search_key_hash_action;
+	SearchKeyHashBehavior::Pointer search_key_hash_behavior;
+
+	SpreadKeyHashAction::Pointer spread_key_hash_action;
+	SpreadKeyHashBehavior::Pointer spread_key_hash_behavior;
+
+	RequestFileAction::Pointer request_file_action;
+	RequestFileBehavior::Pointer request_file_behavior;
+
+	database::FileKeyHashDb::Pointer file_db;
+	neuria::network::NodeId node_id;
+
+	std::ostream& os;
+};
+
+}
+
